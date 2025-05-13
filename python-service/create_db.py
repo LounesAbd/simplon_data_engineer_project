2716682015031cbbb374771b6133b362460e2ce1
Lounes <lounesabdou@ymail.com>
2025-05-14 00:45:07 +0200

    fix: environment issues

--- python-service/create_db.py
+++ python-service/create_db.py
@@ -1,1 +1,4 @@
-print("Hello World")
+# create_db.py
+print("Script create_db.py is running")
+# Ajoutez d'autres instructions pour créer la base de données ici
+print("Script create_db.py has finished")
